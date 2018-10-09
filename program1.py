9701a2ef93a3f7fdb5d01b42be1799dc1ec593d3
Aishwarya-11 <aishwaryalakshmi.s@prodapt.com>
2018-10-09 11:14:36 -0700

    tuple_program

---
+++ program1.py
@@ -0,0 +1,8 @@
+def tuple() :
+	print("Converting tuples to list and inserting an element\n")
+	tup1 = ('a','b','c','d')
+	print tup1
+	list1 = list(tup1)
+	print list1
+	list1.insert(1,'g')
+	print list1
