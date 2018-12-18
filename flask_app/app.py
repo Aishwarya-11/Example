2357474b21da95f2bebf44ad052668bcd78df6f6
Aishwarya-11 <43988490+Aishwarya-11@users.noreply.github.com>
2018-12-18 12:23:06 +0530

    Create app.py

---
+++ flask_app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import assignment
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success",methods=['GET','POST'])
+def success():
+	
+	#if request.method=='POST':
+        	#file_=request.form["file_name"]
+		#with open('myconfig.txt','w') as f:
+			#f.write(file_)
+        assignment.myfunction()
+	return render_template("success.html")
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
